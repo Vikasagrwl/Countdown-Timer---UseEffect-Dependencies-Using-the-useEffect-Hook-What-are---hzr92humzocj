3fe1ddd8adddea19c3d36bff8c103c333099b1d9
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-09 21:34:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,27 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const[timer, setTimer] = useState(0);
+  const interval = setInterval(() => {
+    setTimer(timer-1);
+  }, 1000);
+
+  if(timer<1){
+    clearInterval(interval);
+  }
+
+  useEffect(()=>{
+    return ()=>clearInterval(interval);
+  })
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" type={"number"} onKeyDown={e=>setTimer(e.target.value)} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timer}</div>
     </div>
   )
 }
