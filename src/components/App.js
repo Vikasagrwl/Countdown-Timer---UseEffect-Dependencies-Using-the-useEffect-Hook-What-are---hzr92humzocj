a116dbd9f3a949302e5cdf03df15f5dc0ec4c361
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-03-09 22:02:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
-  const[timer, setTimer] = useState(0);
+  const[timer, setTimer] = useState('0');
   const interval = setInterval(() => {
     setTimer(timer-1);
   }, 1000);
@@ -12,18 +12,22 @@ const App = () => {
     clearInterval(interval);
   }
 
+  const handleKeyDown = (e)=>{
+    setTimer(e.target.value)
+  }
+
   useEffect(()=>{
     return ()=>clearInterval(interval);
-  })
+  },[timer])
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" type={"number"} onKeyDown={e=>setTimer(e.target.value)} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleKeyDown} /> sec.
         </h1>
       </div>
-      <div id="current-time">{timer}</div>
+      <div id="current-time">{!isNaN(timer)? parseInt(timer).toFixed(): '0'}</div>
     </div>
   )
 }
